c8fae727aaa8e65dd8a678064f47515c804a3b3d
Atilla <atillamexmed3@gmail.com>
2023-02-18 08:08:48 +0200

    Initial commit

---
+++ service/src/main/java/me/cal1br/demowebapp/DemoWebAppApplication.java
@@ -0,0 +1,13 @@
+package me.cal1br.demowebapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoWebAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoWebAppApplication.class, args);
+    }
+
+}
